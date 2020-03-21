732aeb054c19e05638fe7cd95c7e2844db920b10
lolitsozzie <ozzie.14.martin@gmail.com>
2020-03-20 22:08:50 -0400

    empty

--- basicapi/blueprints/basicapi_bp.py
+++ basicapi/blueprints/basicapi_bp.py
@@ -32,7 +32,9 @@ def add_user(first_name):
 @basicapi_bp.route('/ezApi', methods=['GET', 'POST'])
 def dataFunction():
     if request.method == 'GET':
-        all_data = [x.value for x in Data.get_all()]
+        all_data = [x for x in Data.get_all()]
+        all_data.sort(key=lambda x: x.id, reverse=False)
+        all_data = [x.value for x in all_data]
         return json.dumps(all_data), 200, {'ContentType': 'application/json'}
     elif request.method == 'POST':
         try:
